790fcb0eb5039acde782384bbc3e1e8a3d46e25d
freddantes <85570101+freddantes@users.noreply.github.com>
2022-02-08 12:09:47 -0300

    Update ExpParser.scala

--- src/main/scala/sewlang/parser/ExpParser.scala
+++ src/main/scala/sewlang/parser/ExpParser.scala
@@ -19,14 +19,21 @@ object ExpParser {
     case SList(List(SSym("+"), exp1, exp2)) => SumExp(parse(exp1), parse(exp2)) // (+ exp1 exp2)
     case SList(List(SSym("*"), exp1, exp2)) => MultExp(parse(exp1), parse(exp2)) // (* exp1 exp2)
     // #5 Implemente o parser para as expressões (- exp1 exp2), (/ exp1 exp2) e (- exp)
+    case SList(List(SSym("-"), exp1, exp2)) => SubExp(parse(exp1), parse(exp2)) // (- exp1 exp2)
+    case SList(List(SSym("/"), exp1, exp2)) => DivExp(parse(exp1), parse(exp2)) // (/ exp1 exp2)
+    case SList(List(SSym("-"), exp)) => NegExp(parse(exp)) // (- exp)
 
     case SList(List(SSym("="), exp1, exp2)) => EqualExp(parse(exp1), parse(exp2)) // (= exp1 exp2)
     case SList(List(SSym("<"), exp1, exp2)) => LessThanExp(parse(exp1), parse(exp2)) // (< exp1 exp2)
     // #6 Implemente o parser para as expressões (<= exp1 exp2), (> exp1 exp2) e (>= exp1 exp2)
+    case SList(List(SSym("<="), exp1, exp2)) => LessOrEqualThanExp(parse(exp1), parse(exp2)) // (<= exp1 exp2)
+    case SList(List(SSym(">"), exp1, exp2)) => LargerThanExp(parse(exp1), parse(exp2)) // (> exp1 exp2)
+    case SList(List(SSym(">="), exp1, exp2)) => LargerOrEqualThanExp(parse(exp1), parse(exp2)) // (>= exp1 exp2)
 
     case SList(List(SSym("not"), exp)) => NotExp(parse(exp)) // (not exp)
     case SList(List(SSym("and"), exp1, exp2)) => AndExp(parse(exp1), parse(exp2)) // (and exp1 exp2)
     // #7 Implemente o parser para a expressão (or exp1 exp2)
+    case SList(List(SSym("or"), exp1, exp2)) => OrExp(parse(exp1), parse(exp2)) // (or exp1 exp2)
 
     case SList(List(SSym("var"), id, exp2)) => VarDeclExp(parse(id), parse(exp2)) // (var id exp)
     case SList(List(SSym("set"), id, exp2)) => VarAssignExp(parse(id), parse(exp2)) // (set id exp)
@@ -41,13 +48,19 @@ object ExpParser {
 
     case SList(List(SSym("read-num"))) => ReadNumExp // (read-num)
     // #8 Implemente o parser para as expressões (read-bool) e (read-str)
+    case SList(List(SSym("read-bool"))) => ReadBoolExp // (read-bool)
+    case SList(List(SSym("read-str"))) => ReadStrExp // (read-str)
 
     // As tarefas devem ser implementadas seguindo a ordem de numeração, logo, as três tarefas abaixo só devem ser implementadas após a #13.
     case SList(List(SSym("++"), id)) => parse(ExpDesugar.desugar(sexp))
     // #14 Implemente o desugar para a expressão (-- id)
+    case SList(List(SSym("--"), id)) => parse(ExpDesugar.desugar(sexp))
 
     case SList(List(SSym("+="), id, exp)) => parse(ExpDesugar.desugar(sexp))
     // #15 Implemente o desugar para as expressões (*= id exp), (-= id exp) e (/= id exp)
+    case SList(List(SSym("*="), id, exp)) => parse(ExpDesugar.desugar(sexp))
+    case SList(List(SSym("-="), id, exp)) => parse(ExpDesugar.desugar(sexp))
+    case SList(List(SSym("/="), id, exp)) => parse(ExpDesugar.desugar(sexp))
 
     case SList(List(SSym("for"), init, cond, mod, body)) => parse(ExpDesugar.desugar(sexp))
     // #16 Implemente o desugar para a expressão (repeat body until-cond)
@@ -65,5 +78,14 @@ object ExpParser {
     val regex = """^[a-zA-Z][a-zA-Z0-9_]*$""".r
     regex.pattern.matcher(str).matches()
   }
+  
+  private def isKeyWord(str: String) = {
+    val KeyWords = Seq("while","var","set","if")
+	if KeyWords.contains(str){
+		println("Identificador não pode usar palavra chave, reveja o nome do identificador!")
+	}else{
+		isIdentifier(str)
+	}
+}
 
 }
